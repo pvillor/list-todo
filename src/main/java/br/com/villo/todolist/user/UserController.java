e32b2dbd2a907e71a2ce21c90bdbae177d1026ab
pvillor <pvillor@gmail.com>
2023-10-11 18:41:06 -0300

    feat: tasks table

--- src/main/java/br/com/villo/todolist/user/UserController.java
+++ src/main/java/br/com/villo/todolist/user/UserController.java
@@ -1,17 +1,36 @@
 package br.com.villo.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userData) {
-    System.out.println(userData.name);
+  public ResponseEntity create(@RequestBody UserModel userData) {
+    var user = this.userRepository.findByUsername(userData.getUsername());
+
+    if(user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+    }
+
+    var hashedPassword = BCrypt.withDefaults().hashToString(12, userData.getPassword().toCharArray());
+
+    userData.setPassword(hashedPassword);
+
+    var userCreated = this.userRepository.save(userData);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 
 }
